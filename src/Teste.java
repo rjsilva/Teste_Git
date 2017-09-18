eefe7cecd576a185c82cc5f2253127e0db024979
Ronaldo Silva <silvarjronaldo@gmail.com>
2017-09-18 16:56:21 -0300

    ajuste na classe teste

--- src/Teste.java
+++ src/Teste.java
@@ -4,7 +4,8 @@ public class Teste {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-			System.out.println("teste");
+		    String hello =  "hello world";
+			System.out.println(hello);
 	}
 
 }
