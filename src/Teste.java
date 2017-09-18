28baaf34af799f086682ee795842d8474d9e1a9f
Ronaldo Silva <silvarjronaldo@gmail.com>
2017-09-18 17:13:53 -0300

    adicionei comentarios

--- src/Teste.java
+++ src/Teste.java
@@ -4,6 +4,7 @@ public class Teste {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		    //teste
 		    String saudacao = "hello world";
 		    String nome = "ronaldo";
 			System.out.println("olá" + nome + " seu primeiro" + saudacao);
